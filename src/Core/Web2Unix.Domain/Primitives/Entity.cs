e21a936111d324801e8b0c3562f1dd41073473d7
Yaroslav Shparuk <Yaroslavs@mwstechnology.com>
2023-02-21 10:56:32 +0200

    Add primitives

---
+++ src/Core/Web2Unix.Domain/Primitives/Entity.cs
@@ -0,0 +1,41 @@
+﻿namespace Web2Unix.Domain.Primitives;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; }
+
+    public static bool operator ==(Entity left, Entity right)
+    {
+        return left is not null && right is not null && left.Equals(right);
+    }
+
+    public static bool operator !=(Entity left, Entity right)
+    {
+        return !(left == right);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType() || obj is not Entity entity)
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
+    }
+
+    public bool Equals(Entity? other)
+    {
+        return other is not null && other.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode() * 5;
+    }
+}
---
+++ src/Core/Web2Unix.Domain/Primitives/ValueObject.cs
@@ -0,0 +1,31 @@
+﻿namespace Web2Unix.Domain.Primitives;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetValues();
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        return obj is ValueObject other && ValuesOfObjectsEqual(other);
+    }
+
+    public bool Equals(ValueObject? other)
+    {
+        return other is not null && ValuesOfObjectsEqual(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return GetValues().Aggregate(default(int), HashCode.Combine);
+    }
+
+    private bool ValuesOfObjectsEqual(ValueObject other)
+    {
+        return GetValues().SequenceEqual(other.GetValues());
+    }
+}
